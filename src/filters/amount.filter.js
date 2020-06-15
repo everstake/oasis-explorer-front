002869a10055bd8d082a1d01b08ab563f68bdacc
muratkarsh <muratkarsh@gmail.com>
2020-06-15 12:11:31 +0300

    fix(accounts list): format to amount

--- src/filters/amount.filter.js
+++ src/filters/amount.filter.js
@@ -1,4 +1,4 @@
 import Vue from 'vue';
 import numeral from 'numeral';
 
-Vue.filter('formatAmount', (amount) => numeral(amount).format('0,0'));
+Vue.filter('formatAmount', (amount) => numeral(amount / 1000000000).format('0,0.000000000'));

78ffb2ec1fc58c5b53236796167eb6a300dc4392
muratkarsh <muratkarsh@gmail.com>
2020-06-22 17:41:42 +0300

    feat(validator): - add tabs with tables according to TR - set fetching limit to 15 items

--- src/views/Validator.vue
+++ src/views/Validator.vue
@@ -10,7 +10,13 @@
           </div>
         </b-col>
       </b-row>
-      <b-row v-else-if="!loading && (Array.isArray(items) && items.length === 0)">
+      <b-row
+        v-else-if="
+        !loading
+        && (Array.isArray(items)
+        && (Array.isArray(tableItems))
+        && items.length === 0)"
+      >
         <b-col cols="12">
           <div class="text-center transaction__empty">
             No data
@@ -116,19 +122,67 @@
                 <div class="validator__actions">
                   <b-btn
                     class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'transfers'
+                    }"
+                    @click="handleTableUpdate('transfers')"
+                  >
+                    Transfers
+                  </b-btn>
+                  <b-btn
+                    class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'addescrow/reclaimescrow'
+                    }"
+                    @click="handleTableUpdate('addescrow/reclaimescrow')"
+                  >
+                    Escrow events
+                  </b-btn>
+                  <b-btn
+                    class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'other'
+                    }"
+                    @click="handleTableUpdate('other')"
+                  >
+                    Other ops
+                  </b-btn>
+                  <b-btn
+                    class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'delegators'
+                    }"
                     @click="handleTableUpdate('delegators')"
                   >
                     Delegators
+                  </b-btn>
+                  <b-btn
+                    class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'performance'
+                    }"
+                    @click="handleTableUpdate('performance')"
+                  >
+                    Validator performance
+                  </b-btn>
+                  <b-btn
+                    class="validator__btn"
+                    :class="{
+                      'validator__btn--active': activeTab === 'charts'
+                    }"
+                    @click="handleTableUpdate('charts')"
+                  >
+                    Charts
                   </b-btn>
                 </div>
                 <div class="validator__shadow">
                   <b-table
                     id="my-table"
-                    :busy="loading && delegators === null"
+                    :busy="loading && tableItems === null"
                     :responsive="true"
                     show-empty
-                    :fields="fields"
-                    :items="delegators"
+                    :fields="getTableFields"
+                    :items="tableItems"
                     class="table table--border table-list validator__table"
                     borderless
                     no-border-collapse
@@ -136,81 +190,89 @@
                     <template #table-busy>
                       <TableLoader />
                     </template>
-                    <template #cell(account_id)="delegators">
-                      <span
-                        v-if="delegators.item.account_id === $route.params.id"
-                      >
-                        {{ delegators.item.account_id }}
-                      </span>
+                    <template #cell(height)="tableItems">
                       <router-link
-                        v-else
-                        :to="{ name: 'account', params: { id: delegators.item.account_id } }"
+                        :to="{ name: 'block', params: { id: tableItems.item.level } }"
                       >
-                        {{ delegators.item.account_name || delegators.item.account_id }}
+                        {{ tableItems.item.level }}
                       </router-link>
                     </template>
-                    <template #cell(escrow_amount)="delegators">
-                      {{ delegators.item.escrow_amount | formatAmount }}
+                    <template #cell(hash)="tableItems">
+                      <router-link
+                        :to="{ name: 'transaction', params: { id: tableItems.item.hash } }"
+                      >
+                        {{ tableItems.item.hash }}
+                      </router-link>
                     </template>
-                    <template #cell(delegate_since)="delegators">
-                      {{ delegators.item.delegate_since | formatYear }}
+                    <template #cell(account_id)="tableItems">
+                      <router-link
+                        :to="{ name: 'account', params: { id: tableItems.item.account_id } }"
+                      >
+                        {{ tableItems.item.account_name || tableItems.item.account_id }}
+                      </router-link>
+                    </template>
+                    <template #cell(escrow_amount)="tableItems">
+                      {{ tableItems.item.escrow_amount | formatAmount }}
+                    </template>
+                    <template #cell(delegate_since)="tableItems">
+                      {{ tableItems.item.delegate_since | formatYear }}
                       <div class="date-from-now">
-                        {{ delegators.item.delegate_since | formatDaysAgo }}
+                        {{ tableItems.item.delegate_since | formatDaysAgo }}
                       </div>
                     </template>
-                    <template #cell(hash)="delegators">
-                      {{ !delegators.item.hash ? '-' : delegators.item.hash }}
+                    <template #cell(hash)="tableItems">
+                      {{ !tableItems.item.hash ? '-' : tableItems.item.hash }}
                     </template>
-                    <template #cell(to)="delegators">
+                    <template #cell(to)="tableItems">
                       <div class="table__hash">
-                        <span v-if="!delegators.item.to">-</span>
+                        <span v-if="!tableItems.item.to">-</span>
                         <span
-                          v-else-if="delegators.item.to ===
+                          v-else-if="tableItems.item.to ===
                           'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA='"
                         >
                           System Account
                         </span>
                         <router-link
                           v-else
-                          :to="{ name: 'account', params: { id: delegators.item.to } }"
+                          :to="{ name: 'account', params: { id: tableItems.item.to } }"
                         >
-                          {{ delegators.item.to }}
+                          {{ tableItems.item.to }}
                         </router-link>
                       </div>
                     </template>
-                    <template #cell(from)="delegators" class="table__hash">
+                    <template #cell(from)="tableItems" class="table__hash">
                       <div class="table__hash">
-                        <span v-if="!delegators.item.from">-</span>
+                        <span v-if="!tableItems.item.from">-</span>
                         <router-link
                           v-else
-                          :to="{ name: 'account', params: { id: delegators.item.from } }"
+                          :to="{ name: 'account', params: { id: tableItems.item.from } }"
                         >
-                          {{ delegators.item.from }}
+                          {{ tableItems.item.from }}
                         </router-link>
                       </div>
                     </template>
-                    <template #cell(proposer)="delegators">
-                      {{ !delegators.item.proposer ? '-' : delegators.item.proposer }}
+                    <template #cell(proposer)="tableItems">
+                      {{ !tableItems.item.proposer ? '-' : tableItems.item.proposer }}
                     </template>
-                    <template #cell(timestamp)="delegators">
-                      {{ delegators.item.timestamp | formatDate }}
+                    <template #cell(timestamp)="tableItems">
+                      {{ tableItems.item.timestamp | formatDate }}
                     </template>
-                    <template #cell(amount)="delegators">
-                      {{ delegators.item.amount | formatAmount }}
+                    <template #cell(amount)="tableItems">
+                      {{ tableItems.item.amount | formatAmount }}
                     </template>
-                    <template #cell(storage)="delegators">
-                      {{ !delegators.item.storage ? '-' : delegators.item.storage }}
+                    <template #cell(storage)="tableItems">
+                      {{ !tableItems.item.storage ? '-' : tableItems.item.storage }}
                     </template>
-                    <template #cell(gas_used)="delegators">
-                      {{ !delegators.item.gas_used ? '-' : delegators.item.gas_used }}
+                    <template #cell(gas_used)="tableItems">
+                      {{ !tableItems.item.gas_used ? '-' : tableItems.item.gas_used }}
                     </template>
-                    <template #cell(fees)="delegators">
-                      {{ !delegators.item.fees ? '-' : delegators.item.fees }}
+                    <template #cell(fees)="tableItems">
+                      {{ !tableItems.item.fees ? '-' : tableItems.item.fees }}
                     </template>
                   </b-table>
                   <b-pagination
                     v-model="currentPage"
-                    :total-rows="delegators.length"
+                    :total-rows="tableItems.length"
                     :per-page="perPage"
                     aria-controls="my-table"
                   ></b-pagination>
@@ -227,6 +289,7 @@
 import Breadcrumbs from '@/components/Breadcrumbs.vue';
 import TableLoader from '@/components/TableLoader.vue';
 import copyToClipboard from '@/mixins/copyToClipboard';
+/* eslint-disable */
 
 export default {
   name: 'Validator',
@@ -239,30 +302,10 @@ export default {
   ],
   data() {
     return {
-      data: {
-        blockHeight: 0,
-        blockHash: 0,
-        epoch: 0,
-        timestamp: 0,
-        blockTime: 0,
-        proposer: 0,
-        numOfSignatures: 0,
-        gas: 0,
-        fees: 0,
-        from: 0,
-        to: 0,
-        type: null,
-        amount: 0,
-      },
       loading: null,
       limit: 50,
       items: null,
       delegators: [],
-      fields: [
-        { key: 'account_id', label: 'Account' },
-        { key: 'escrow_amount', label: 'Escrow amount', sortable: true },
-        { key: 'delegate_since', label: 'Delegate since', sortable: true },
-      ],
       breadcrumbs: [
         {
           toRouteName: 'home',
@@ -279,18 +322,167 @@ export default {
       ],
       currentPage: 1,
       perPage: 5,
+      transfers: null,
+      otherOps: null,
+      activeTab: 'transfers',
+      tableItems: [],
+      charts: {
+        uptime: null,
+        stake: null,
+      },
     };
   },
   methods: {
-    handleTableUpdate(list) {
-      console.log('list', list);
+    setActiveTab(label) {
+      this.activeTab = label;
+    },
+    handleTableUpdate(type) {
+      switch (type) {
+        case 'transfers':
+          this.getValidatorTransfers();
+          this.setActiveTab('transfers');
+          break;
+        case 'delegators':
+          this.getValidatorDelegators();
+          this.setActiveTab('delegators');
+          break;
+        case 'addescrow/reclaimescrow':
+          this.getValidatorEscrowEvents();
+          this.setActiveTab('addescrow/reclaimescrow');
+          break;
+        case 'performance':
+          this.getValidatorUptime();
+          this.setActiveTab('performance');
+          break;
+        case 'charts':
+          this.getValidatorCharts();
+          this.setActiveTab('charts');
+          break;
+        case 'other':
+          this.getValidatorOtherOps();
+          this.setActiveTab('other');
+          break;
+        default:
+          this.getValidatorTransfers();
+          this.setActiveTab('transfers');
+      }
+    },
+    async getValidatorTransfers() {
+      this.loading = true;
+      const transfers = await this.$api.getTransactions({
+        limit: 15,
+        sender: this.$route.params.id,
+        receiver: this.$route.params.id,
+      });
+
+      this.tableItems = transfers.data;
+      this.loading = false;
+    },
+    async getValidatorEscrowEvents() {
+      this.loading = true;
+      const transfers = await this.$api.getTransactions({
+        limit: 15,
+        operation_kind: ['addescrow', 'reclaimescrow'],
+      });
+
+      this.tableItems = transfers.data;
+      this.loading = false;
+    },
+    async getValidatorOtherOps() {
+      this.loading = true;
+      const otherOps = await this.$api.getTransactions({
+        limit: 15,
+        operation_kind: ['registernode', 'registerentity', 'amendcommissionschedule', 'registerruntime'],
+      });
+
+      this.tableItems = otherOps.data;
+      this.loading = false;
+    },
+    async getValidatorDelegators() {
+      this.loading = true;
+      const delegators = await this.$api.getValidatorDelegators({
+        limit: 15,
+        id: this.$route.params.id,
+      });
+
+      this.tableItems = delegators.data;
+      this.loading = false;
+    },
+    getValidatorCharts() {
+      this.getValidatorUptime();
+      this.getValidatorStake();
+    },
+    async getValidatorUptime() {
+      this.loading = true;
+      const todayMs = new Date().getTime();
+      const thirtyDaysMs = new Date(new Date().setDate(new Date().getDate() - 30)).getTime();
+
+      const todaySec = Math.round(todayMs / 1000);
+      const thirtyDaysSec = Math.round(thirtyDaysMs / 1000);
+
+      const uptime = await this.$api.getValidatorUptime({
+        limit: 15,
+        from: thirtyDaysSec,
+        to: todaySec,
+        frame: 'D',
+        id: this.$route.params.id,
+      });
+
+      this.charts.uptime = uptime.data;
+      this.loading = false;
+    },
+    async getValidatorStake() {
+      this.loading = true;
+      const todayMs = new Date().getTime();
+      const thirtyDaysMs = new Date(new Date().setDate(new Date().getDate() - 30)).getTime();
+
+      const todaySec = Math.round(todayMs / 1000);
+      const thirtyDaysSec = Math.round(thirtyDaysMs / 1000);
+
+      const stake = await this.$api.getValidatorStake({
+        limit: 15,
+        from: thirtyDaysSec,
+        to: todaySec,
+        frame: 'D',
+        id: this.$route.params.id,
+      });
+
+      this.charts.stake = stake.data;
+      this.loading = false;
+    },
+  },
+  computed: {
+    getTableFields() {
+      if (this.activeTab === 'transfers') {
+        return [
+          { key: 'level', label: 'Height' },
+          { key: 'hash', label: 'Transaction hash' },
+          { key: 'from', label: 'From' },
+          { key: 'to', label: 'To' },
+          { key: 'amount', label: 'Amount' },
+          { key: 'timestamp', label: 'Date' },
+        ];
+      }
+
+      if (this.activeTab === 'addescrow/reclaimescrow') {
+        return [
+          { key: 'level', label: 'Height' },
+          { key: 'hash', label: 'Operation hash' },
+          { key: 'type', label: 'Type' },
+          { key: 'amount', label: 'Amount' },
+          { key: 'timestamp', label: 'Date' },
+        ];
+        return;
+      }
+      
+      return [];
     },
   },
   async created() {
     this.loading = true;
 
     const data = await this.$api.getValidator({
-      limit: 50,
+      limit: 15,
       id: this.$route.params.id,
     });
 
@@ -298,6 +490,8 @@ export default {
       this.$router.push({ name: '404' });
     }
     this.items = data.data;
+
+    this.getValidatorTransfers();
 
     this.loading = false;
   },
@@ -331,8 +525,13 @@ export default {
   }
 
   &__btn {
+    margin-right: 15px;
     background-color: #4cd4a9 !important;
     border-color: #4cd4a9 !important;
+
+    &--active {
+      box-shadow: 0 0 0 0.2rem rgba(76, 212, 169, .5) !important;
+    }
 
     &:focus,
     &:active,

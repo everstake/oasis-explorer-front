4eb482d5891ec658b7a54af092863380dd31e250
muratkarsh <muratkarsh@gmail.com>
2020-07-21 15:00:11 +0300

    feature(jest): configurations

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{js,vue}',
-  ],
-  coveragePathIgnorePatterns: [
-    'src/main.js',
-  ],
   setupFiles: [
     './tests/config/setupTests.js',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+  ],
+  transform: {
+    '.*\\.(vue)$': 'vue-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
+  coveragePathIgnorePatterns: [
+    'src/main.js',
   ],
 };

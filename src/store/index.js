ecebed811e3789333ba5c231b4c0ac873cb372d2
muratkarsh <muratkarsh@gmail.com>
2020-07-27 15:29:08 +0300

    refactor(store): split and export

--- src/store/index.js
+++ src/store/index.js
@@ -5,22 +5,34 @@ Vue.use(Vuex);
 
 const defaultDateFormat = localStorage.getItem('dateFormat') || Vue.prototype.$constants.DATE_FORMAT;
 
+const state = {
+  height: null,
+  dateFormat: defaultDateFormat,
+};
+
+const mutations = {
+  setInfo(state, payload) {
+    state.height = payload.height;
+  },
+  setDateFormat(state, payload) {
+    localStorage.setItem('dateFormat', payload);
+    state.dateFormat = payload;
+  },
+};
+
+const actions = {};
+const modules = {};
+
 export default new Vuex.Store({
-  state: {
-    height: null,
-    dateFormat: defaultDateFormat,
-  },
-  mutations: {
-    setInfo(state, payload) {
-      state.height = payload.height;
-    },
-    setDateFormat(state, payload) {
-      localStorage.setItem('dateFormat', payload);
-      state.dateFormat = payload;
-    },
-  },
-  actions: {
-  },
-  modules: {
-  },
+  state,
+  mutations,
+  actions,
+  modules,
 });
+
+export {
+  state,
+  mutations,
+  actions,
+  modules,
+}

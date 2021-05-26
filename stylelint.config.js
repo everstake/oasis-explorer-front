37d31a399c648b851526e357039d507fbbb4dd44
Vladimir-Beliy <vovaazick@gmail.com>
2021-05-26 12:11:45 +0300

    add project settings

---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'stylelint-config-recommended-scss',
+  rules: {
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
+  },
+  ignoreFiles: ['**/*.{png,svg}'],
+};

e46bbc81430da47da2e071197b94f78880477d78
Vladimir <60401137+Vladimir-Beliy@users.noreply.github.com>
2021-05-31 10:46:23 +0300

    update filter formatAmount (#78)

--- src/filters/amount.filter.js
+++ src/filters/amount.filter.js
@@ -2,9 +2,20 @@ import Vue from 'vue';
 import numeral from 'numeral';
 
 const formatAmount = (amount) => {
+  if (typeof amount !== 'number') {
+    return '-';
+  }
+
   if (amount <= 1) {
     return `${amount}`;
   }
+
+  if (amount < 1000) {
+    return (amount / 1000000000)
+      .toFixed(9)
+      .replace(/0*$/, '');
+  }
+
   return numeral(amount / 1000000000).format('0,0.[000000000]');
 };
 

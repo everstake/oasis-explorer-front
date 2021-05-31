987fc453d61f73a9ec57807a1eb2810deb70435a
Vladimir <60401137+Vladimir-Beliy@users.noreply.github.com>
2021-05-31 10:42:07 +0300

    packages update (#73)

--- tests/config/jest.init.js
+++ tests/config/jest.init.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
 
 import '@/plugins/constants.plugin';
 import '@/filters/date.filter';
@@ -16,7 +15,7 @@ import { config } from '@vue/test-utils';
 
 Vue.use(BootstrapVue);
 
-library.add(fas, fab, far);
+library.add(fas, fab);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 

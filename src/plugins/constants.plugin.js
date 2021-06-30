caabdbc23bc9ca39725a5dc2eb60b93079fb8f6d
Vladimir <60401137+Vladimir-Beliy@users.noreply.github.com>
2021-06-30 13:48:20 +0300

    add network switcher (#80)
    * add network switcher
    
    * Update TopLine.vue
    
    * Update .env.sample

--- src/plugins/constants.plugin.js
+++ src/plugins/constants.plugin.js
@@ -1,9 +1,18 @@
 import Vue from 'vue';
 
+const networksTypes = {
+  mainnet: 'mainnet',
+  testnet: 'testnet',
+};
+
 const constants = {
   DATE_FORMAT: 'HH:mm DD.MM.YYYY',
   DATE_FORMAT_US: 'hh:mm A MM.DD.YYYY',
   SYSTEM_ACCOUNT_ID: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=',
+  MAINNET_URL: process.env.VUE_APP_MAINNET_URL,
+  TESTNET_URL: process.env.VUE_APP_TESTNET_URL,
+  NETWORK_TYPE: process.env.VUE_APP_NETWORK_TYPE,
+  NETWORK_TYPES: networksTypes,
 };
 
 Vue.use({

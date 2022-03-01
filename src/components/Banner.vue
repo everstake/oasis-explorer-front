5e78384d37e6e0a6aaca2f507ccb4b392cffb817
Mikle_D <mda@atticlab.net>
2022-03-01 18:40:59 +0200

    add banner

---
+++ src/components/Banner.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="['banner']">
+    <b-container>
+      <a
+        href="https://nation.io/dao/ukraine"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src="~@/assets/images/banner.jpg"
+          :class="['image', 'img-war']"
+        >
+        <img
+          src="~@/assets/images/banner-mobile.jpg"
+          :class="['image', 'img-war-mobile']"
+        >
+      </a>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+};
+</script>
+
+<style scoped>
+.banner {
+  width: 100%;
+}
+
+.image {
+  width: 100%;
+}
+
+@media (max-width: 767px) {
+  .img-war {
+    display: none;
+  }
+}
+@media (min-width: 767px) {
+  .img-war-mobile {
+    display: none;
+  }
+}
+</style>

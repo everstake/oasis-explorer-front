bda730bbaa3064409993c4440f063d30d085873c
yevheniimolodyk <y.molodyk@everstake.one>
2023-04-07 16:17:51 +0300

    hardcode env urls in code to have single docker image

--- src/plugins/constants.plugin.js
+++ src/plugins/constants.plugin.js
@@ -5,13 +5,48 @@ const networksTypes = {
   testnet: 'testnet',
 };
 
+const env = {
+  API_URL: '',
+  MAINNET_URL: '',
+  TESTNET_URL: '',
+  NETWORK_TYPE: '',
+};
+
+if (window.location.href.includes('oasismonitor.com')) {
+  env.API_URL = 'https://api.oasismonitor.com';
+  env.NETWORK_TYPE = 'mainnet';
+  env.MAINNET_URL = 'https://oasismonitor.com';
+  env.TESTNET_URL = 'https://testnet.oasismonitor.com';
+} else if (window.location.href.includes('stage.oasismonitor.com')) {
+  env.API_URL = 'https://api-stage.oasismonitor.com';
+  env.NETWORK_TYPE = 'mainnet';
+  env.MAINNET_URL = 'https://stage.oasismonitor.com';
+  env.TESTNET_URL = 'https://testnet-stage.oasismonitor.com';
+} else if (window.location.href.includes('testnet.oasismonitor.com')) {
+  env.API_URL = 'https://testnet-api.oasismonitor.com';
+  env.NETWORK_TYPE = 'testnet';
+  env.MAINNET_URL = 'https://oasismonitor.com';
+  env.TESTNET_URL = 'https://testnet.oasismonitor.com';
+} else if (window.location.href.includes('testnet-stage.oasismonitor.com')) {
+  env.API_URL = 'https://testnet-api-stage.oasismonitor.com';
+  env.NETWORK_TYPE = 'testnet';
+  env.MAINNET_URL = 'https://stage.oasismonitor.com';
+  env.TESTNET_URL = 'https://testnet-stage.oasismonitor.com';
+} else {
+  env.API_URL = 'https://api.oasismonitor.com';
+  env.NETWORK_TYPE = 'mainnet';
+  env.MAINNET_URL = 'https://oasismonitor.com';
+  env.TESTNET_URL = 'https://testnet.oasismonitor.com';
+}
+
 const constants = {
   DATE_FORMAT: 'HH:mm DD.MM.YYYY',
   DATE_FORMAT_US: 'hh:mm A MM.DD.YYYY',
   SYSTEM_ACCOUNT_ID: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=',
-  MAINNET_URL: process.env.VUE_APP_MAINNET_URL,
-  TESTNET_URL: process.env.VUE_APP_TESTNET_URL,
-  NETWORK_TYPE: process.env.VUE_APP_NETWORK_TYPE,
+  API_URL: env.API_URL,
+  MAINNET_URL: env.MAINNET_URL,
+  TESTNET_URL: env.TESTNET_URL,
+  NETWORK_TYPE: env.NETWORK_TYPE,
   NETWORK_TYPES: networksTypes,
 };
 
@@ -20,3 +55,5 @@ Vue.use({
     Vue.prototype.$constants = constants;
   },
 });
+
+export default constants;
